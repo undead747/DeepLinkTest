4c5a83b57dfbe482d753f1d336ab5fa05d71dc1d
unknown <minh.trancong@NSK.LOCAL>
2023-02-22 14:28:25 +0900

    update

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 if (/iPhone/i.test(navigator.userAgent)) {
-    alert("iphone")
+    window.location.replace("instagram://");
+
+    setTimeout(() => {
+      window.location.replace(
+        "https://apps.apple.com/us/app/instagram/id389801252"
+      );
+    },100);
 }
 
 if (/Android/i.test(navigator.userAgent)) {
-    alert("Android")
+    const url =
+    "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
+
+  window.location.replace(url);
 }
 
   
